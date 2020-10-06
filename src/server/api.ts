b4b2ae398e25b82ac5a1c8ff8a67eb8032cb5a32
Vitaliy Berekchiyan <v.berekchiyan@semrush.com>
2020-10-06 16:19:36 +0300

    Revert "add puppeteer"
    This reverts commit f64ca7d476cdc6e5eaf242be54db330a4730664b.

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import getCurrentDayNamesRequest from "../request/getDayNamesRequest";
 import getNewsRequest from "../request/getNewsRequest";
-import puppeteer from "puppeteer";
 
 const router = Router();
 
@@ -32,29 +31,6 @@ router.get("/time", (_req, res) => {
     day: date.getDate(),
     month: date.getMonth() + 1,
   });
-});
-
-router.get("/test", async (_req, res, next) => {
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setUserAgent(
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
-    );
-    await page.goto("http://kakoysegodnyaprazdnik.ru/");
-
-    const dayNames = await page.evaluate(() =>
-      Array.from(
-        document.querySelectorAll('span[itemprop="text"]')
-      ).map((node) => node.textContent?.split("(")[0].trim())
-    );
-
-    await browser.close();
-
-    res.json(dayNames);
-  } catch (error) {
-    next({ error });
-  }
 });
 
 export default router;
