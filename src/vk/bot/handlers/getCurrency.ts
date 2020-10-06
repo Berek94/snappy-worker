f3a3cfa86ef1557971d2b9906f063020a59afef1
Vitaliy Berekchiyan <v.berekchiyan@semrush.com>
2020-10-06 22:26:11 +0300

    change currency command

--- src/vk/bot/handlers/getCurrency.ts
+++ src/vk/bot/handlers/getCurrency.ts
@@ -1,18 +1,18 @@
 import FetchError from "../../../fetch/FetchError";
-import { formatCurrency } from "../../../helpers";
-import currencyRequest from "../../../request/getCurrencyRequest";
+// import { formatCurrency } from "../../../helpers";
+// import currencyRequest from "../../../request/getCurrencyRequest";
 import { CommandHandler } from "../VkBot";
 
 const getCurrency: CommandHandler = async (ctx) => {
   try {
-    const { usd, eur } = await currencyRequest();
+    // const { usd, eur } = await currencyRequest();
 
-    const text = [
-      `💵 Доллар: ${formatCurrency(usd.value, "USD")}`,
-      `💶 Евро: ${formatCurrency(eur.value, "EUR")}`,
-    ].join("\n");
+    // const text = [
+    //   `💵 Доллар: ${formatCurrency(usd.value, "USD")}`,
+    //   `💶 Евро: ${formatCurrency(eur.value, "EUR")}`,
+    // ].join("\n");
 
-    ctx.reply(text);
+    ctx.reply("https://snappy-worker.ru/screenshot.png");
   } catch (error: unknown) {
     if (error instanceof FetchError) {
       ctx.reply(error.message);
